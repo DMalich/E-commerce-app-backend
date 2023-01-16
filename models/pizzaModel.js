0bd46f76aa535c702837deda52f8747e77f77c7b
DMalich <d.malich@gmail.com>
2023-01-16 17:54:34 +0200

    adding to github

---
+++ models/pizzaModel.js
@@ -0,0 +1,19 @@
+const mongoose = require("mongoose");
+
+const pizzaSchema = new mongoose.Schema(
+    {
+        name: { type: String, require },
+        sizes: [],
+        prices: [],
+        category: { type: String, require },
+        image: { type: String, require },
+        desc: { type: String, require },
+    },
+    {
+        timestamps: true,
+    }
+);
+
+const pizzaModel = mongoose.model("pizzas", pizzaSchema);
+
+module.exports = pizzaModel;
---
+++ server.js
@@ -0,0 +1,39 @@
+const express = require("express");
+const mongoose = require("mongoose");
+
+const pizzaModel = require("./models/pizzaModel.js");
+const pizzasRoute = require("./routes/pizzaRoute.js");
+const userModel = require("./models/userModel.js");
+const userRoute = require("./routes/userRoute.js");
+const orderRoute = require("./routes/orderRoute.js");
+
+
+const MONGODB_URI =
+    "mongodb+srv://mongo:24dmdm24@cluster0.swvnicn.mongodb.net/e-commerce-pizza";
+mongoose
+    .connect(MONGODB_URI)
+    .then(() => {
+        console.log("connected to DB");
+    })
+    .catch((err) => {
+        console.log("Error:");
+        console.log(err.message);
+    });
+
+const app = express();
+
+app.use(express.json());
+
+app.use('/api/pizzas/', pizzasRoute); 
+app.use('/api/users/', userRoute);
+app.use('/api/orders/', orderRoute);
+
+app.get("/", (req, res) => {
+    res.send("server is running");
+});
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => {
+    console.log(`server is running on port ${port}`);
+});
